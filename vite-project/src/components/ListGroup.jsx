268b73e75a041b1c9198e4458e5ffa2966fce40d
weijiej2964 <weijie.jiang09@myhunter.cuny.edu>
2023-11-21 15:29:35 -0500

    Alert and ListGroup (Tutorial tinkering)

--- vite-project/src/components/ListGroup.jsx
+++ vite-project/src/components/ListGroup.jsx
@@ -1,13 +1,29 @@
-function ListGroup() {
+import { useState } from "react";
+
+function ListGroup({ items, heading, OnSelectItem }) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
-      <h1>List</h1>
-      <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              OnSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
