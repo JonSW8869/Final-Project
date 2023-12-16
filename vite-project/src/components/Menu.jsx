914aec47d01859d1d1e4d3ec5c3589f16095b444
weijiej2964 <weijie.jiang09@myhunter.cuny.edu>
2023-12-16 15:52:01 -0500

    episide ver basic setup

--- vite-project/src/components/Menu.jsx
+++ vite-project/src/components/Menu.jsx
@@ -3,19 +3,58 @@ import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div>
-      <h1>Welcome to the app! Click on an episode above.</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/episode/1">Episode 1</Link>
-          </li>
-          <li>
-            <Link to="/episode/2">Episode 2</Link>
-          </li>
-          {/* Add more links for other episodes */}
-        </ul>
+    <div style={{ backgroundColor: "#e3f2fd", minHeight: "100vh" }}>
+      {/* Navbar */}
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            C++ Adventures
+          </Link>
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/episode/1">
+                Episode Ver. Prototype
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/episode/2">
+                Episode Ver. Basic Setup
+              </Link>
+            </li>
+            {/* Add more nav-item elements for additional episodes */}
+          </ul>
+        </div>
       </nav>
+
+      {/* Main Content */}
+      <div className="container mt-3 d-flex justify-content-around">
+        {/* Episode Cards */}
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Episode Ver. Prototype</h5>
+            <p className="card-text">
+              This is a Prototype episode during development, feel free to
+              visit.
+            </p>
+            <Link to="/episode/1" className="btn btn-primary">
+              Start Episode
+            </Link>
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Episode Ver. Basic Setup</h5>
+            <p className="card-text">
+              Explore the basics of C++ setup with George in this episode.
+            </p>
+            <Link to="/episode/2" className="btn btn-primary">
+              Start Episode
+            </Link>
+          </div>
+        </div>
+        {/* Add more card elements for additional episodes */}
+      </div>
     </div>
   );
 };
